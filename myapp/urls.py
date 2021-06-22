ec96dba9bc495f8b4073443f0b4aa6484a128144
Yuta Tomimasu <t1910444@edu.cc.uec.ac.jp>
2021-06-22 21:03:11 +0900

    'update'

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ app_name = 'myapp'
 
 urlpatterns = [
     path('templates/', views.index_template, name='index_template'),
-    path('', views.upload, name='upload'),
+    path('upload', views.upload, name='upload'),
     path('csv_export/', views.csv_export, name='csv_export'),
     path('upload_complete/', views.upload_complete, name='upload_complete'),
 ]
